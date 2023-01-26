51aa37af5f0e52ddf291f3cce0cd1baa0f8c5cbf
Janita <bgj369@myy.haaga-helia.fi>
2023-01-26 12:22:09 +0200

    added api request for adding new task

--- src/fetch/user/dao.js
+++ src/fetch/user/dao.js
@@ -35,7 +35,7 @@ const getUserTasks = async (jwt) => {
       },
     });
     const response = await fetch(request);
-    console.log("response", response);
+    // console.log("response", response);
     if (response.status === 400) {
       return 400;
     }
@@ -45,7 +45,35 @@ const getUserTasks = async (jwt) => {
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log("error", error);
+    // console.log("error", error);
+    return "error" + error;
+  }
+};
+
+const postNewTask = async (jwt, newTask) => {
+  try {
+    const request = new Request("http://localhost:3001/api/task/post", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(newTask),
+    });
+    const response = await fetch(request);
+    // console.log("response", response);
+    if (response.status === 400) {
+      return 400;
+    }
+
+    if (response.status === 500) {
+      return 500;
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    // console.log("error", error);
     return "error" + error;
   }
 };
@@ -53,5 +81,6 @@ const getUserTasks = async (jwt) => {
 const dao = {
   postLogin,
   getUserTasks,
+  postNewTask,
 };
 export default dao;
